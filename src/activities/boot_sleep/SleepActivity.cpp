ce2ad0cfb4e0414559913a55d003511a87426d9a
Jonas Diemer <jonasdiemer@gmail.com>
2025-12-31 11:53:25 +0100

    Show sleep popup with bold border and text.

--- src/activities/boot_sleep/SleepActivity.cpp
+++ src/activities/boot_sleep/SleepActivity.cpp
@@ -42,16 +42,16 @@ void SleepActivity::onEnter() {
 }
 
 void SleepActivity::renderPopup(const char* message) const {
-  const int textWidth = renderer.getTextWidth(UI_12_FONT_ID, message);
+  const int textWidth = renderer.getTextWidth(UI_12_FONT_ID, message, BOLD);
   constexpr int margin = 20;
   const int x = (renderer.getScreenWidth() - textWidth - margin * 2) / 2;
   constexpr int y = 117;
   const int w = textWidth + margin * 2;
   const int h = renderer.getLineHeight(UI_12_FONT_ID) + margin * 2;
   // renderer.clearScreen();
-  renderer.fillRect(x + 5, y + 5, w - 10, h - 10, true);
-  renderer.drawText(UI_12_FONT_ID, x + margin, y + margin, message, false);
-  renderer.drawRect(x + 5, y + 5, w - 10, h - 10);
+  renderer.fillRect(x - 5, y - 5, w + 10, h + 10, true);
+  renderer.fillRect(x + 5, y + 5, w - 10, h - 10, false);
+  renderer.drawText(UI_12_FONT_ID, x + margin, y + margin, message, true, BOLD);
   renderer.displayBuffer();
 }
 
